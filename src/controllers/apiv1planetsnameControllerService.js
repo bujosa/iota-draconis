242e845f2887020d4ba587e947c8db1cbb546598
bujosa <davidbujosa@gmail.com>
2022-02-06 03:37:32 +0100

    Add new functions on controller

--- src/controllers/apiv1planetsnameControllerService.js
+++ src/controllers/apiv1planetsnameControllerService.js
@@ -1,19 +1,111 @@
-'use strict'
+"use strict";
+
+var db = require("../db");
 
 module.exports.findPlanetByname = function findPlanetByname(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for findPlanetByname'
-  });
+  var name = req.name.value;
+  if (!name) {
+    console.warn(
+      "New GET request to /planets/:name without name, sending 400..."
+    );
+    res.sendStatus(400); // bad request
+  } else {
+    console.info("New GET request to /planets/" + name);
+    db.find({ name: name }, function (err, filteredPlanets) {
+      if (err) {
+        console.error("Error getting data from DB");
+        res.sendStatus(500); // internal server error
+      } else {
+        if (filteredPlanets.length > 0) {
+          var planet = filteredPlanets[0]; //since we expect to have exactly ONE planet with this name
+          console.info("Sending planet: " + JSON.stringify(planet, 2, null));
+          res.send(planet);
+        } else {
+          console.warn("There are no planets with name " + name);
+          res.sendStatus(404); // not found
+        }
+      }
+    });
+  }
 };
 
 module.exports.deletePlanet = function deletePlanet(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for deletePlanet'
-  });
+  var name = req.name.value;
+  if (!name) {
+    console.warn(
+      "New DELETE request to /planets/:name without name, sending 400..."
+    );
+    res.sendStatus(400); // bad request
+  } else {
+    console.info("New DELETE request to /planets/" + name);
+    db.remove({ name: name }, function (err, numRemoved) {
+      if (err) {
+        console.error("Error removing data from DB");
+        res.sendStatus(500); // internal server error
+      } else {
+        console.info("Planets removed: " + numRemoved);
+        if (numRemoved === 1) {
+          console.info(
+            "The planet with name " +
+              name +
+              " has been succesfully deleted, sending 204..."
+          );
+          res.sendStatus(204); // no content
+        } else {
+          console.warn("There are no planets to delete");
+          res.sendStatus(404); // not found
+        }
+      }
+    });
+  }
 };
 
 module.exports.updatePlanet = function updatePlanet(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for updatePlanet'
-  });
+  var updatedPlanet = req.planet.value;
+  var name = req.name.value;
+  if (!updatedPlanet) {
+    console.warn("New PUT request to /planets/ without planet, sending 400...");
+    res.sendStatus(400); // bad request
+  } else {
+    console.info(
+      "New PUT request to /planets/" +
+        name +
+        " with data " +
+        JSON.stringify(updatedPlanet, 2, null)
+    );
+    if (
+      !updatedPlanet.name ||
+      !updatedPlanet.satellite ||
+      !updatedPlanet.haveWater ||
+      !updatedPlanet.orbitalPeriod
+    ) {
+      console.warn(
+        "The planet " +
+          JSON.stringify(updatedPlanet, 2, null) +
+          " is not well-formed, sending 422..."
+      );
+      res.sendStatus(422); // unprocessable entity
+    } else {
+      db.find({ name: updatedPlanet.name }, function (err, planets) {
+        if (err) {
+          console.error("Error getting data from DB");
+          res.sendStatus(500); // internal server error
+        } else {
+          if (planets.length > 0) {
+            db.update({ name: name }, updatedPlanet);
+            console.info(
+              "Modifying planet with name " +
+                name +
+                " with data " +
+                JSON.stringify(updatedPlanet, 2, null)
+            );
+            res.sendStatus(204); // no content
+          } else {
+            console.warn("There are not any planet with name " + name);
+            res.sendStatus(404); // not found
+          }
+        }
+      });
+    }
+  }
 };
