c1ca246b6005610116d194175a6f8aa0c7728558
David Bujosa <davidbujosa@gmail.com>
2022-02-23 05:02:26 +0100

    feat: add frontend settings

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 # development
 export NODE_ENV=development
-export PORT=8008
+export BACK_PORT=4000
 export DBPORT=27018
-docker-compose -p planets-dev up -d --build
+export FRONT_PORT=3000
+
+docker-compose -p planet-dev up -d --build 
 
 # production
 export NODE_ENV=production
-export PORT=8001
-export DBPORT=27011
-docker-compose -p planets-prod up -d --build
+export BACK_PORT=4001
+export DBPORT=27017
+export FRONT_PORT=3001
+docker-compose -p planet-prod up -d --build
--- stop.sh
+++ stop.sh
@@ -2,10 +2,10 @@
 export NODE_ENV=development
 export PORT=8008
 export DBPORT=27018
-docker-compose -p planets-dev down
+docker-compose -p planet-dev down
 
 # production
 export NODE_ENV=production
 export PORT=8001
 export DBPORT=27011
-docker-compose -p planets-prod down
+docker-compose -p planet-prod down
