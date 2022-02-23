da57a297da3562416ed4799cf452c1d9e446fad5
David Bujosa <davidbujosa@gmail.com>
2022-02-23 01:31:17 +0100

    feat: add stop

---
+++ stop.sh
@@ -0,0 +1,11 @@
+# development
+export NODE_ENV=development
+export PORT=8008
+export DBPORT=27018
+docker-compose -p planets-dev down
+
+# production
+export NODE_ENV=production
+export PORT=8001
+export DBPORT=27011
+docker-compose -p planets-prod down
