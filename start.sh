f16bd749574630ccb5e96ec854ef28ec5dd3307c
David Bujosa <davidbujosa@gmail.com>
2022-03-06 08:00:50 +0100

    fix: changes port

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ docker-compose -p planet-dev up -d --build
 # production
 export NODE_ENV=production
 export BACK_PORT=4001
-export DBPORT=27017
+export DBPORT=27019
 export FRONT_PORT=3001
 docker-compose -p planet-prod up -d --build
