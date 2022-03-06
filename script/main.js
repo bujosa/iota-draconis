45d334f3c7dbf5082403dccb2f98e77faa538670
David Bujosa <davidbujosa@gmail.com>
2022-03-06 13:30:41 +0100

    feat: add main script

---
+++ script/main.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+      vus: 1,
+      iterations: 3,
+};
+
+export default function () {
+  const url = `http://localhost:8080/api/v1/planets/analytics/100000`;
+
+  const res = http.get(url);
+
+  check(res, { "is status 200": (r) => r.status === 200 });
+
+  sleep(0.3);
+}
