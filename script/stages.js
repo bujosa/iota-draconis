d899103fdf46d42c1536c4fc90279e01e1aa4ae0
David Bujosa <davidbujosa@gmail.com>
2022-03-09 07:16:40 +0100

    feat: add stages

---
+++ script/stages.js
@@ -0,0 +1,50 @@
+export const stages = [
+  {
+    name: "|2-100|2-100|2-0|",
+    config: [
+      { duration: "2m", target: 100 },
+      { duration: "2m", target: 100 },
+      { duration: "2m", target: 0 },
+    ],
+  },
+  {
+    name: "|2-500|2-500|2-0|",
+    config: [
+      { duration: "2m", target: 500 },
+      { duration: "2m", target: 500 },
+      { duration: "2m", target: 0 },
+    ],
+  },
+  {
+    name: "|2-1000|2-1000|2-0|",
+    config: [
+      { duration: "2m", target: 1000 },
+      { duration: "2m", target: 1000 },
+      { duration: "2m", target: 0 },
+    ],
+  },
+  {
+    name: "|5-100|10-100|5-0|",
+    config: [
+      { duration: "5m", target: 100 },
+      { duration: "10m", target: 100 },
+      { duration: "5m", target: 0 },
+    ],
+  },
+  {
+    name: "|5-500|10-500|5-0|",
+    config: [
+      { duration: "5m", target: 500 },
+      { duration: "10m", target: 500 },
+      { duration: "5m", target: 0 },
+    ],
+  },
+  {
+    name: "|5-1000|10-1000|5-0|",
+    config: [
+      { duration: "5m", target: 1000 },
+      { duration: "10m", target: 1000 },
+      { duration: "5m", target: 0 },
+    ],
+  },
+];
